73fb6cb64ef48e6ba707b3d8fbac1332a8033b12
dubeyswastik72 <dubeyshubh2042@gmail.com>
2026-07-03 15:06:21 +0530

    OOPS Lec 1

--- src/OOPS/FirstLecture.java
+++ src/OOPS/FirstLecture.java
@@ -1,17 +1,32 @@
 package OOPS;
+import java.util.Arrays;
+import java.util.Scanner;
 
+//NOTES:
 //class - a named group of properties and functions or a blueprint of objects, theoretical/logical construct
 //objects - an instance/reality/practical representation of a class
 //Objects originally occupy space in the memory
+//Instance variable - variables declared inside the class but outside the constructors and methods
+//dot (.) operator - links the instance variable with the reference name of the object
+//new operator - creates an object or dynamically(at runtime) allocates memory at runtime and returns the reference to it
+//When we create an object of a class then the class is instantiated
 
 class Student{
-    String name;
-    int roll_no;
-    double marks;
+    String name;            //instance variable1
+    int roll_no;            //instance variable2
+    double marks;           //instance variable3
 };
 
 public class FirstLecture {
     public static void main(String[] args){
-
+        Student s1;     //s1 is the reference variable
+                        //This is just declaring the reference to the object and not the creation of object
+        s1 = new Student();     //object created using new
+        s1.name = "Swastik";
+        s1.roll_no = 43;
+        s1.marks = 43;
+        System.out.println(s1.name);
+        System.out.println(s1.roll_no);
+        System.out.println(s1.marks);
     }
 }

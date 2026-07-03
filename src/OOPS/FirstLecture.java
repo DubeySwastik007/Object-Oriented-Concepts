823959141e2fff2776da528efa1f7227c26d2bb1
dubeyswastik72 <dubeyshubh2042@gmail.com>
2026-07-03 13:37:00 +0530

    OOPS Lec 1

---
+++ src/OOPS/FirstLecture.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+//class - a named group of properties and functions or a blueprint of objects, theoretical/logical construct
+//objects - an instance/reality/practical representation of a class
+//Objects originally occupy space in the memory
+
+class Student{
+    String name;
+    int roll_no;
+    double marks;
+};
+
+public class FirstLecture {
+    public static void main(String[] args){
+
+    }
+}

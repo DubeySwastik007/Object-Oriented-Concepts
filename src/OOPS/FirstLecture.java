79fe77da3ac1ee276617effb39bc2a0bb92509aa
dubeyswastik72 <dubeyshubh2042@gmail.com>
2026-07-03 15:50:05 +0530

    Basics of objects and classes

--- src/OOPS/FirstLecture.java
+++ src/OOPS/FirstLecture.java
@@ -6,22 +6,26 @@ import java.util.Scanner;
 //Constructor - a special function in a class used to initialize values to object at its creation time
 //When no constructor is created then a default constructor is used which initialize the instance variable to null or 0
 //constructor is invoked at the time of object creation
+//this - points to the current object
+
 
 class Student{
     String name;
     int roll_no;
     double marks;
-    Student(String str, int rno, double mrks){
-        System.out.println("Constructor Invoked");
-        name = str;
-        roll_no = rno;
-        marks = mrks;
+    //this is not default constructor
+    Student(){
+        this.name = "Aksh Dubey";
+        this.roll_no = 43;
+        this.marks = 432.5;
     }
 };
 
 public class FirstLecture {
     public static void main(String[] args){
-        Student s1 = new Student("Swastik", 3, 43);
+        Student s1 = new Student();
         System.out.println(s1.name + " " + s1.roll_no + " " + s1.marks);
+        Student s2 = new Student();
+        System.out.println(s2.name + " " + s2.roll_no + " " + s2.marks);
     }
 }

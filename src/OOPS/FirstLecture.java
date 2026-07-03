d8b821d12cf55af0328d3f7fbf97f1c6713a74cb
dubeyswastik72 <dubeyshubh2042@gmail.com>
2026-07-03 16:39:39 +0530

    Wrapper Classes

--- src/OOPS/FirstLecture.java
+++ src/OOPS/FirstLecture.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class FirstLecture {
     public static void main(String[] args){
-//        WRAPPER CLASSES
-        Scanner sc = new Scanner(System.in);
-        int a = 1;   //a is not an object
-//        OR
-        Integer num = new Integer(sc.nextInt());  //num is an object of type integer
-        System.out.println(a + " " + num);
-        Integer num1 = sc.nextInt();  //or Integer num1 = 10;     //here the value of object is either user defined or 10
-        System.out.println(num1);
+        //This swap program does not changes the value because the Integer class is a final class
+        Scanner in = new Scanner(System.in);
+        Integer a = in.nextInt();
+        Integer b = in.nextInt();
+        System.out.println(a + " " + b);
+        swap(a, b);
+        System.out.println(a + " " + b);
+    }
+    static void swap(Integer a, Integer b){
+        Integer temp = a;
+        a = b;
+        b = temp;
     }
 }
